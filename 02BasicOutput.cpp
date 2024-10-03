3bef46bc3c3f461cef4367b98a4a0eddcac5de38
Chavez Ruvelyn <chavezruvelyn@gmail.com>
2024-10-03 09:58:44 +0800

    PROGRAMMING ACTIVITY 02

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "Bachelor of Science in Information Technology 1st Year" << endl;
+	cout << "Ruvelyn Pajarin Chavez" << endl;
+	cout << "P-1 Macogon Labo Camarines Norte 4604" <<endl;
+	return 0;
+}
