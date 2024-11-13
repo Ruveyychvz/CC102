3aa02a44d90615bab891e192c49c70089804b34a
Chavez Ruvelyn <chavezruvelyn@gmail.com>
2024-11-13 08:57:47 +0800

    Add files via upload
    Age

---
+++ Activity2_Age.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int age;
+	cout << "What is your age?";
+	cin >> age;
+	
+	if (age < 16) {
+		cout << "Too young to drive";
+	}
+	else if (age == 16) {
+			cout << "Better clear the road";
+	}
+	else if (age > 16) {
+				cout << "You are getting pretty old";
+	}
+	return 0;
+}
+		
+		
